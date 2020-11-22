c5fe9941b9acaeea035529778b4781c2fac5aef1
ammar <ammarkaan@gmail.com>
2020-11-22 09:21:33 +0500

    Second Commit

---
+++ build/precache-manifest.462c92dedffe2aec6a8dacaafe3fbfd9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b73ab19152d25e2bbd2a8ed92cfc4f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "340ac627ffc77d264656",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f07e0cb973d88c090f3f",
+    "url": "/static/css/main.22f8a389.chunk.css"
+  },
+  {
+    "revision": "340ac627ffc77d264656",
+    "url": "/static/js/2.c3702a97.chunk.js"
+  },
+  {
+    "revision": "f71ab56469abf8363df2dca5f19589ab",
+    "url": "/static/js/2.c3702a97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f07e0cb973d88c090f3f",
+    "url": "/static/js/main.94b01806.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "a89e1ec7a5eaec00073ca5c2426fa32c",
+    "url": "/static/media/background.a89e1ec7.jpg"
+  },
+  {
+    "revision": "91a960a04d76d57b14dbe3a6d25176ab",
+    "url": "/static/media/camera.91a960a0.png"
+  }
+]);
